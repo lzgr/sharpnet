d3de902075cd1750c5f560ed058e7abea06effac
lzgr <luka.zgrablic@racunarstvo.hr>
2018-01-25 00:01:51 +0100

    Changed layout of elements in subnet mask list

--- Sharpnet/list_masks.Designer.cs
+++ Sharpnet/list_masks.Designer.cs
@@ -29,41 +29,21 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(list_masks));
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.mainTable = new System.Windows.Forms.TableLayoutPanel();
             this.SuspendLayout();
             // 
-            // flowLayoutPanel1
+            // mainTable
             // 
-            this.flowLayoutPanel1.AutoSize = true;
-            this.flowLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 50);
-            this.flowLayoutPanel1.MaximumSize = new System.Drawing.Size(967, 0);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(0, 5, 0, 0);
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(736, 5);
-            this.flowLayoutPanel1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(8, 21);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(261, 23);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "List of all IPv4 subnet masks";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(599, 8);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(139, 36);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Main menu";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.mainTable.AutoSize = true;
+            this.mainTable.ColumnCount = 1;
+            this.mainTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.mainTable.Location = new System.Drawing.Point(12, 12);
+            this.mainTable.Name = "mainTable";
+            this.mainTable.RowCount = 2;
+            this.mainTable.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.mainTable.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.mainTable.Size = new System.Drawing.Size(848, 589);
+            this.mainTable.TabIndex = 0;
             // 
             // list_masks
             // 
@@ -71,9 +51,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(872, 613);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.mainTable);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "list_masks";
             this.Text = "Sharpnet";
@@ -84,8 +63,6 @@
 
         #endregion
 
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel mainTable;
     }
 }
--- Sharpnet/list_masks.cs
+++ Sharpnet/list_masks.cs
@@ -27,7 +27,32 @@ namespace Sharpnet
             sub_mask.o2 = 0;
             sub_mask.o3 = 0;
             sub_mask.o4 = 0;
+
+            Button button1 = new Button();
+            button1.Text = "Main menu";
+            button1.Size = new Size(136, 32);
+            Label desc = new Label();
+            desc.AutoSize = true;
+            desc.Text = "List of all IPv4 subnet masks";
+            desc.Font = new Font(FontFamily.GenericSansSerif, 10.2F);
             
+            mainTable.AutoSize = true;
+            FlowLayoutPanel flowLayoutPanel1 = new FlowLayoutPanel();
+            flowLayoutPanel1.AutoSize = true;
+            flowLayoutPanel1.MaximumSize = new Size(850, 0);
+            mainTable.Controls.Add(flowLayoutPanel1, 0, 1);
+            flowLayoutPanel1.Anchor = (AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top);
+
+            TableLayoutPanel tableLayoutPanel1 = new TableLayoutPanel();
+            mainTable.Controls.Add(tableLayoutPanel1, 0, 0);
+            tableLayoutPanel1.Height = 35;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle() { Width = 50, SizeType = SizeType.Percent });
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle() { Width = 50, SizeType = SizeType.Percent });
+            tableLayoutPanel1.Controls.Add(desc, 0, 0);
+            tableLayoutPanel1.Controls.Add(button1, 1, 0);
+            desc.Anchor = AnchorStyles.Left;
+            button1.Anchor = AnchorStyles.Right;
+
             Label labela0 = new Label();
             labela0.Text = Iptotext(0, sub_mask);
             labela0.AutoSize = true;
@@ -46,6 +71,7 @@ namespace Sharpnet
                 flowLayoutPanel1.Controls.Add(labela);
             }
 
+            tableLayoutPanel1.Width = flowLayoutPanel1.Width;
         }
 
         public string Iptotext(uint iter, maska sub)
